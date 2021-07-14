6eca9b5a3caa5488deef70eb0aa7c517e1c3f5f9
AlazaziAmr <alazaziamr@gmail.com>
2021-07-14 18:10:31 +0300

    Nothing happens

--- app/Http/Controllers/SilController.php
+++ app/Http/Controllers/SilController.php
@@ -1,15 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
-require_once 'vendor/autoload.php';
-
-
 use Illuminate\Http\Request;
 use Silamoney\Client\Api\SilaApi;
 use Silamoney\Client\Domain\{BalanceEnvironments,Environments};
 
-     $appHandle = "kdfkd";
 class SilController extends Controller
 {
+    
 }
