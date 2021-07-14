7a19b43bd68ec46efbe1dba7722065de5b069c74
Hameed Ahmed <ahmed.abbas.look@gmail.com>
2021-07-14 16:32:00 +0300

    Added authintaction and email verification

--- app/Http/Controllers/SilController.php
+++ app/Http/Controllers/SilController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+require_once 'vendor/autoload.php';
+
+
 use Illuminate\Http\Request;
 use Silamoney\Client\Api\SilaApi;
 use Silamoney\Client\Domain\{BalanceEnvironments,Environments};
 
+     $appHandle = "kdfkd";
 class SilController extends Controller
 {
-    //
 }
